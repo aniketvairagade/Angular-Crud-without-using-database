4423876e0ee7daf896c5c2ea0a7c70bdb9af0ff3
aniketvairagade <aniketvairagade2000@gmail.com>
2023-02-02 19:17:54 +0530

    Create crud in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatTableModule} from '@angular/material/table';
+import {MatDialogModule} from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateComponent } from './create/create.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { AddEditUserComponent } from './add-edit-user/add-edit-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    HomeComponent,
+    UserComponent,
+    AddEditUserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatButtonModule,
+    AppRoutingModule,
+    MatIconModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTableModule,
+    MatDialogModule,
+    MatInputModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/user/user.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { AddEditUserComponent } from '../add-edit-user/add-edit-user.component';
+// import { UserService } from '../../services/user.service';
+import { UserServiceService } from '../user-service.service';
+
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent implements OnInit {
+  displayedColumns: string[] = ['Id','First Name', 'Last Name', 'Email', 'Contact','Address', 'Action'];
+  userDetails!: MatTableDataSource<any>
+
+  constructor(
+    public dialog: MatDialog,
+    public userService: UserServiceService,
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  private getUsers() {
+    this.userDetails = new MatTableDataSource<Element>(this.userService.getUser());
+  }
+
+  public openPopup() {
+    const dialogRef = this.dialog.open(AddEditUserComponent, {
+      width: '350px',
+      data: {}
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      this.getUsers();
+    });
+  }
+
+  public editUser(index: any, data: any) {
+    data.id = index;
+    data.action = 'edit';
+    const dialogRef = this.dialog.open(AddEditUserComponent, {
+      width: '350px',
+      data: data
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      this.getUsers();
+    });
+  }
+
+  public deleteUser(index: any) {
+    const data = {
+      action: 'delete'
+    }
+    const dialogRef = this.dialog.open(AddEditUserComponent, {
+      width: '400px',
+      data: data
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.userService.deleteUser(index);
+        this.getUsers();
+      }
+    });
+  }
+}
